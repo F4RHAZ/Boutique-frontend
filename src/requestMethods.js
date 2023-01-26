db78b8b766beddde45cdbc4ce670a8db0511a016
F4RHAZ <farhazamyn1@gmail.com>
2023-01-27 02:21:57 +0300

    changes to API call

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const BASE_URL = "https://a-zboutiqueapi.onrender.com/api/";
+const BASE_URL = "https://a-zboutiqueapi.onrender.com/api/";
 
-const BASE_URL = "http://127.0.0.1:5000/api/"
+//const BASE_URL = "http://127.0.0.1:5000/api/"
 const root = JSON.parse(localStorage.getItem("persist:root"));
 const user = root ? root.user : null;
 const currentUser = user ? JSON.parse(user).currentUser : null;
