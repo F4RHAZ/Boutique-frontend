17c6b148b076928f624d1142d7e0b81769a72571
F4RHAZ <farhazamyn1@gmail.com>
2023-02-09 00:38:59 +0300

    axios correction

--- src/components/PayButton.jsx
+++ src/components/PayButton.jsx
@@ -24,7 +24,7 @@ const PayButton = ({cart}) =>{
     const handleCheckout =() =>{
         let cart1 = cart.products;
         let userId = user._id
-        axios.post(`https://a-zboutiquefrontend.onrender.com/api/checkout/webhook`, {
+        axios.post(`https://a-zboutiquefrontend.onrender.com/api/checkout/create-checkout-session`, {
             userId,
             cart1, 
         // axios.post(`http://127.0.0.1:5000/api/checkout/create-checkout-session`, {
